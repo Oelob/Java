9f958f9dd74559cbab96dc665c736775de321bdc
Ivan <ab09@inbox.ru>
2023-02-23 02:04:43 +0500

    completed sorted module

--- sem03_homework/homework.java
+++ sem03_homework/homework.java
@@ -6,7 +6,9 @@ import java.util.Random;
 
 public class homework {
     public static void main(String[] args) {
-        int[] randMassive = getMassive(10, 20);
+        int[] randMassive = getMassive(5, 20);
+        System.out.println(Arrays.toString(randMassive));
+        sortedMassive(randMassive);
         System.out.println(Arrays.toString(randMassive));
         
 
@@ -21,11 +23,44 @@ public class homework {
         return result;
     }
 
-    public static int[] sortedMassive(int[] array) {
-        int[] result = new int[array.length];
+    public static void sortedMassive(int[] array) {
+        if (array.length > 1) {
+
+        int midle = array.length/2;
+        int[] left_massive = new int [midle];
         
-        return result;
+        for (int i = 0; i < midle; i++){
+            left_massive[i] = array[i];
+        }
+        
+        int[] right_massive = new int [array.length-midle];
+        for (int i = 0; i < right_massive.length; i++){
+                right_massive[i] = array[i+midle];
+        }
+        sortedMassive(left_massive);
+        sortedMassive(right_massive);
+        
+        int left_count = 0;
+        int right_count = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+                if (right_count >= right_massive.length || (left_count < left_massive.length && left_massive[left_count] <= right_massive[right_count])) {
+                    array[i] = left_massive[left_count];
+                    left_count++;
+                } else {
+                    array[i] = right_massive[right_count];
+                    right_count++;
+                }
+            
+            }
+        }  
     }
-        
 }
+        
+       
+
+
+    
+        
+
  
