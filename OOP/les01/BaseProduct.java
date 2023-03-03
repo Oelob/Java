f9a2d4b2a931078ec848d1d71770cd60e623ee48
Ivan <ab09@inbox.ru>
2023-03-03 13:46:55 +0500

    added data into BaseProduct

--- OOP/les01/BaseProduct.java
+++ OOP/les01/BaseProduct.java
@@ -1,5 +1,26 @@
 package OOP.les01;
 
+import sem01_homework.program;
+
 public class BaseProduct {
-    
+    private String name;
+    private double price;
+    private int count;
+    private String unitMeasure;
+
+    public BaseProduct(String name, double price, int count, String unitMeaString){
+        this.name = name;
+        this.price = price;
+        this.count = count;
+        this.unitMeasure = unitMeaString;
+    }
+
+    public String getInfo(){
+        return String.format("Name: %s Price %.2f Count %d UnitMeasure %s",
+        this.name, this.price, this.count, this.unitMeasure);
+    }
+
+    public String toString(){
+        return getInfo();
+    }
 }
