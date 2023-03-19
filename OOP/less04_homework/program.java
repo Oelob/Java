e3c6348bb3f727f88639e4f95570dede7e95e86f
Ivan <ab09@inbox.ru>
2023-03-20 01:12:16 +0500

    deleted some files

--- OOP/less04_homework/program.java
+++ OOP/less04_homework/program.java
@@ -9,17 +9,6 @@ import OOP.less_figures.Treangle;
 
 public class program {
     public static void main(String[] args) throws SumException {
-        // String[] array = new String[]{"володя","петя","иван","андрей","аааа","аааб"};
-        // Rectangle rectangle = new Rectangle("прямоугольник", 2, 4);
-        // Treangle treangle = new Treangle("треугольник", 2, 3, 4);
-        // Circle circle = new Circle("михаил", 3);
-        // Circle circle2 = new Circle("михаил", 5);
-        // Figures storage = new Figures();
-        // storage.addFig(circle);
-        // storage.addFig(circle2);
-        // Circle[] array5 = new Circle[]{circle,circle2};
-        
-        
         
         Integer[] array = new Integer[] {5,2,4,1,2,3,5,4};
         // String[] array = new String[]{"15"," 13","15","12","аааа","аааб"};
